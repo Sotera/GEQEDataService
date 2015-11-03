5c944be3cd673b96ef1eeeb826de8cd026d6d8d8
Luke Wendling <luke@lukewendling.com>
2015-11-03 16:53:35 -0600

    touchups

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var mongoAddr = '"' + process.env.MONGO_PORT_27017_TCP_ADDR + '"';
-var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT;
+var mongoAddr = process.env.MONGO_PORT_27017_TCP_ADDR||"localhost";
+var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT||"27017";
 var fs = require('fs');
 
+// create datasources for docker deployments
 var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
-fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
-fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
+fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, '"' + mongoAddr + '"');
+fileContents = fileContents.replace(/MONGO_HOST_PORT/g, '"' + mongoPort + '"');
 fs.writeFileSync(__dirname + '/datasources.json', fileContents);
 
 var loopback = require('loopback');
